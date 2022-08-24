2100ad2b0042a0216931554d8240a6a6798b4ee9
Stark <serj.elessar@gmail.com>
2022-08-24 11:41:03 +0300

    create UserDto

---
+++ dtos/userDto.js
@@ -0,0 +1,12 @@
+//DTO - Data Transfer Object
+module.exports = class UserDto {
+  email
+  id
+  isActivated
+
+  constructor(model) {
+    this.email = model.email
+    this.id = model._id
+    this.isActivated = model.isActivated
+  }
+}
